7a6d80e7c37e32f627d0d9e157e03bed160d729d
KerrieMeister96 <koensmit13@gmail.com>
2025-02-16 23:59:44 +0100

    routing updated

--- src/pages/Learning.js
+++ src/pages/Learning.js
@@ -3,7 +3,6 @@ import { FaRegLightbulb } from "react-icons/fa";
 const Learning = () => {
   return (
     <div className="flex flex-col items-center justify-start text-white min-h-screen p-6">
-      {/* Page Header */}
       <div className="text-center">
         <h1 className="text-5xl font-bold flex items-center justify-center text-green-500">
           Learning
